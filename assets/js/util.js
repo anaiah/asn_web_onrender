7755e0dc0e4b05b863564f303fef8fdaa3ba0a2c
anaiah <carlodominguez@yahoo.com>
2025-02-11 10:02:53 +0800

    feb11 v2

--- assets/js/util.js
+++ assets/js/util.js
@@ -6,9 +6,9 @@ this is for utilities
 modals,forms,utilities
 
 */ 
-//const myIp = "https://asn-api.onrender.com"
+const myIp = "https://asn-api.onrender.com"
 
-const myIp = "http://192.168.191.221:10000"
+//const myIp = "http://192.168.191.221:10000"
 
 const requirements = document.querySelectorAll(".requirements")
 const specialChars = "!@#$%^&*()-_=+[{]}\\| :'\",<.>/?`~"
